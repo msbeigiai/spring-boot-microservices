ba18e1b726e903b3226ce0a92d362684638dcef5
msbeigiai <msbeigi83@gmail.com>
2023-06-26 14:58:52 +0330

    Add itegration test for diffrent services

---
+++ OrderService/src/test/java/com/msbeigi/orderservice/controller/OrderControllerTest.java
@@ -0,0 +1,56 @@
+package com.msbeigi.orderservice.controller;
+
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.client.WireMockBuilder;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import com.msbeigi.orderservice.OrderServiceConfig;
+import com.msbeigi.orderservice.repository.OrderRepository;
+import com.msbeigi.orderservice.services.OrderService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest({"server.port=0"})
+@EnableConfigurationProperties
+@AutoConfigureMockMvc
+@ContextConfiguration(classes = {OrderServiceConfig.class})
+public class OrderControllerTest {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @RegisterExtension
+    static WireMockExtension wireMockExtension =
+            WireMockExtension.newInstance()
+                    .options(WireMockConfiguration.wireMockConfig().port(8080))
+                    .build();
+
+    private ObjectMapper objectMapper = new ObjectMapper()
+            .findAndRegisterModules()
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+    @Test
+    public void test_WhenPlaceOrder_DoPayment_Success() {
+        // First place order
+        // Get order by order id from db
+        // check output
+    }
+}
---
+++ OrderService/src/test/java/com/msbeigi/orderservice/services/OrderServiceImplTest.java
@@ -0,0 +1,198 @@
+package com.msbeigi.orderservice.services;
+
+import com.msbeigi.orderservice.entity.Order;
+import com.msbeigi.orderservice.exception.CustomException;
+import com.msbeigi.orderservice.external.client.PaymentService;
+import com.msbeigi.orderservice.external.client.ProductService;
+import com.msbeigi.orderservice.external.request.PaymentRequest;
+import com.msbeigi.orderservice.external.response.PaymentResponse;
+import com.msbeigi.orderservice.external.response.ProductResponse;
+import com.msbeigi.orderservice.model.OrderRequest;
+import com.msbeigi.orderservice.model.OrderResponse;
+import com.msbeigi.orderservice.model.PaymentMode;
+import com.msbeigi.orderservice.repository.OrderRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Instant;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class OrderServiceImplTest {
+
+    @Mock
+    private OrderRepository orderRepository;
+    @Mock
+    private ProductService productService;
+    @Mock
+    private PaymentService paymentService;
+    @Mock
+    private RestTemplate restTemplate;
+
+    @InjectMocks
+    OrderService orderService = new OrderServiceImpl();
+
+    @DisplayName("Get order - success scenario")
+    @Test
+    void test_When_Order_Success() {
+        // Mocking
+        Order order = getMockOrder();
+        when(orderRepository.findById(anyLong()))
+                .thenReturn(Optional.of(order));
+        when(restTemplate.getForObject(
+                "http://PRODUCT-SERVICE/product/" + order.getProductId(),
+                ProductResponse.class
+        )).thenReturn(getMockProductResponse());
+
+        when(restTemplate.getForObject(
+                "http://PAYMENT-SERVICE/payment/order/" + order.getId(),
+                PaymentResponse.class
+        )).thenReturn(getMockPaymentResponse());
+
+        // Actual
+        OrderResponse orderResponse = orderService.getOrderDetailsById(1);
+
+        // Verification
+        verify(orderRepository, times(1))
+                .findById(anyLong());
+        verify(restTemplate, times(1)).getForObject(
+                "http://PRODUCT-SERVICE/product/" + order.getProductId(),
+                ProductResponse.class);
+        verify(restTemplate, times(1)).getForObject(
+                "http://PAYMENT-SERVICE/payment/order/" + order.getId(),
+                PaymentResponse.class);
+
+        // Assert
+        assertNotNull(orderResponse);
+        assertEquals(order.getId(), orderResponse.getOrderId());
+    }
+
+    @DisplayName("Get Orders - Failure scenario")
+    @Test
+    void test_When_Get_Order_Not_Found_Then_Not_Found() {
+
+        when(orderRepository.findById(anyLong()))
+                .thenReturn(Optional.empty());
+
+        CustomException customException =
+                assertThrows(CustomException.class,
+                        () -> orderService.getOrderDetailsById(1));
+        assertEquals("NOT_FOUND", customException.getErrorCode());
+        assertEquals(404, customException.getStatus());
+
+        verify(orderRepository, times(1))
+                .findById(anyLong());
+
+    }
+
+    @DisplayName("Place order - Success Scenario")
+    @Test
+    void test_When_Place_Order_Success() {
+
+        Order order = getMockOrder();
+        OrderRequest orderRequest = getMockOrderRequest();
+
+        when(orderRepository.save(any(Order.class)))
+                .thenReturn(order);
+        when(productService.reduceQuantity(anyLong(), anyLong()))
+                .thenReturn(new ResponseEntity<Void>(HttpStatus.OK));
+        when(paymentService.doPayment(any(PaymentRequest.class)))
+                .thenReturn(new ResponseEntity<Long>(1L, HttpStatus.OK));
+
+        long orderId = orderService.placeOrder(orderRequest);
+
+        verify(orderRepository, times(2))
+                .save(any());
+        verify(productService, times(1))
+                .reduceQuantity(anyLong(), anyLong());
+        verify(paymentService, times(1))
+                .doPayment(any(PaymentRequest.class));
+
+        assertEquals(order.getId(), orderId);
+
+    }
+
+    @DisplayName("Place Order - Payment fails scenario")
+    @Test
+    void test_When_Place_Order_Payment_Fails_then_Order_Places() {
+        Order order = getMockOrder();
+        OrderRequest orderRequest = getMockOrderRequest();
+
+        when(orderRepository.save(any(Order.class)))
+                .thenReturn(order);
+        when(productService.reduceQuantity(anyLong(), anyLong()))
+                .thenReturn(new ResponseEntity<Void>(HttpStatus.OK));
+        when(paymentService.doPayment(any(PaymentRequest.class)))
+                .thenThrow(new RuntimeException());
+
+        long orderId = orderService.placeOrder(orderRequest);
+
+        verify(orderRepository, times(2))
+                .save(any());
+        verify(productService, times(1))
+                .reduceQuantity(anyLong(), anyLong());
+        verify(paymentService, times(1))
+                .doPayment(any(PaymentRequest.class));
+
+        assertEquals(order.getId(), orderId);
+
+    }
+
+    private OrderRequest getMockOrderRequest() {
+        return OrderRequest
+                .builder()
+                .productId(1)
+                .quantity(10)
+                .paymentMode(PaymentMode.CASH)
+                .totalAmount(100)
+                .build();
+    }
+
+    private PaymentResponse getMockPaymentResponse() {
+        return PaymentResponse
+                .builder()
+                .paymentId(1)
+                .paymentDate(Instant.now())
+                .paymentMode(PaymentMode.CASH)
+                .amount(200)
+                .orderId(1)
+                .status("ACCEPTED")
+                .build();
+    }
+
+    private ProductResponse getMockProductResponse() {
+        return ProductResponse
+                .builder()
+                .productId(2)
+                .productName("iPhone")
+                .price(400)
+                .quantity(200)
+                .build();
+    }
+
+    private Order getMockOrder() {
+        return Order.builder()
+                .orderStatus("PLACED")
+                .orderDate(Instant.now())
+                .id(1)
+                .amount(100)
+                .quantity(200)
+                .productId(2)
+                .build();
+    }
+
+}
