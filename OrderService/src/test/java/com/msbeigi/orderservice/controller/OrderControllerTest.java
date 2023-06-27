974fc621842db912b6052450404583e8001cbffd
msbeigiai <msbeigi83@gmail.com>
2023-06-27 16:14:40 +0330

    Dockeriz microservices

--- OrderService/src/test/java/com/msbeigi/orderservice/controller/OrderControllerTest.java
+++ OrderService/src/test/java/com/msbeigi/orderservice/controller/OrderControllerTest.java
@@ -59,18 +59,22 @@ public class OrderControllerTest {
     private CircuitBreakerRegistry circuitBreakerRegistry;
 
     @RegisterExtension
-    static WireMockExtension wireMockServer =
-            WireMockExtension.newInstance()
-                    .options(WireMockConfiguration.wireMockConfig().port(8088))
-                    .build();
+    static WireMockExtension wireMockServer
+            = WireMockExtension.newInstance()
+            .options(WireMockConfiguration
+                    .wireMockConfig()
+                    .port(8088))
+            .build();
 
-    private ObjectMapper objectMapper = new ObjectMapper()
+    private ObjectMapper objectMapper
+            = new ObjectMapper()
             .findAndRegisterModules()
             .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
+
     @BeforeEach
-    void setUp() throws IOException {
+    void setup() throws IOException {
         getProductDetailsResponse();
         doPayment();
         getPaymentDetails();
@@ -109,6 +113,7 @@ public class OrderControllerTest {
     }
 
     private void getProductDetailsResponse() throws IOException {
+        // GET /product/1
         wireMockServer.stubFor(get("/product/1")
                 .willReturn(aResponse()
                         .withStatus(HttpStatus.OK.value())
@@ -118,15 +123,14 @@ public class OrderControllerTest {
                                         .getClassLoader()
                                         .getResourceAsStream("mock/GetProduct.json"),
                                 defaultCharset()
-                        ))
-                )
-        );
+                        ))));
+
 
     }
 
+
     private OrderRequest getMockOrderRequest() {
-        return OrderRequest
-                .builder()
+        return OrderRequest.builder()
                 .productId(1)
                 .paymentMode(PaymentMode.CASH)
                 .quantity(10)
@@ -136,17 +140,17 @@ public class OrderControllerTest {
 
     @Test
     public void test_WhenPlaceOrder_DoPayment_Success() throws Exception {
-        // First place order
-        // Get order by order id from db
-        // check output
+        //First Place Order
+        // Get Order by Order Id from Db and check
+        //Check Output
+
         OrderRequest orderRequest = getMockOrderRequest();
-        MvcResult mvcResult =
-                mockMvc.perform(MockMvcRequestBuilders.post("/order/placeOrder")
-                        .with(jwt()
-                                .authorities(new SimpleGrantedAuthority("Customer")))
+        MvcResult mvcResult
+                = mockMvc.perform(MockMvcRequestBuilders.post("/order/placeOrder")
+                        .with(jwt().authorities(new SimpleGrantedAuthority("Customer")))
                         .contentType(MediaType.APPLICATION_JSON_VALUE)
-                        .content(objectMapper.writeValueAsString(orderRequest)))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+                        .content(objectMapper.writeValueAsString(orderRequest))
+                ).andExpect(MockMvcResultMatchers.status().isOk())
                 .andReturn();
 
         String orderId = mvcResult.getResponse().getContentAsString();
@@ -155,81 +159,85 @@ public class OrderControllerTest {
         assertTrue(order.isPresent());
 
         Order o = order.get();
+
         assertEquals(Long.parseLong(orderId), o.getId());
         assertEquals("PLACED", o.getOrderStatus());
         assertEquals(orderRequest.getTotalAmount(), o.getAmount());
         assertEquals(orderRequest.getQuantity(), o.getQuantity());
+
     }
 
     @Test
-    public void test_When_Place_Order_With_Wrong_Access_ThenThrow403() throws Exception {
+    public void test_WhenPlaceOrderWithWrongAccess_thenThrow403() throws Exception {
         OrderRequest orderRequest = getMockOrderRequest();
-        MvcResult mvcResult =
-                mockMvc.perform(MockMvcRequestBuilders.post("/order/placeOrder")
-                                .with(jwt()
-                                        .authorities(new SimpleGrantedAuthority("Admin")))
-                                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                                .content(objectMapper.writeValueAsString(orderRequest)))
-                        .andExpect(MockMvcResultMatchers.status().isForbidden())
-                        .andReturn();
+        MvcResult mvcResult
+                = mockMvc.perform(MockMvcRequestBuilders.post("/order/placeOrder")
+                        .with(jwt().authorities(new SimpleGrantedAuthority("Admin")))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(orderRequest))
+                ).andExpect(MockMvcResultMatchers.status().isForbidden())
+                .andReturn();
     }
 
-    @Test
-    public void test_When_Get_Order_Is_Success() throws Exception {
 
-        MvcResult mvcResult =
-                mockMvc.perform(MockMvcRequestBuilders.get("/order/1")
-                                .with(jwt().authorities(new SimpleGrantedAuthority("Admin")))
-                                .contentType(MediaType.APPLICATION_JSON_VALUE))
-                        .andExpect(MockMvcResultMatchers.status().isOk())
-                        .andReturn();
+    /*@Test
+    public void test_WhenGetOrder_Success() throws Exception {
+        MvcResult mvcResult
+                = mockMvc.perform(MockMvcRequestBuilders.get("/order/1")
+                        .with(jwt().authorities(new SimpleGrantedAuthority("Admin")))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
 
         String actualResponse = mvcResult.getResponse().getContentAsString();
         Order order = orderRepository.findById(1L).get();
         String expectedResponse = getOrderResponse(order);
 
+
         assertEquals(expectedResponse, actualResponse);
+    }*/
+
+    @Test
+    public void testWhen_GetOrder_Order_Not_Found() throws Exception {
+        MvcResult mvcResult
+                = mockMvc.perform(MockMvcRequestBuilders.get("/order/100")
+                        .with(jwt().authorities(new SimpleGrantedAuthority("Admin")))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andReturn();
     }
 
     private String getOrderResponse(Order order) throws IOException {
-        OrderResponse.PaymentDetails paymentDetails =
-                objectMapper.readValue(
-                        copyToString(
-                                OrderControllerTest.class.getClassLoader()
-                                        .getResourceAsStream("mock/GetPayment.json"),
-                                defaultCharset()
-                        ), OrderResponse.PaymentDetails.class
-                );
+        OrderResponse.PaymentDetails paymentDetails
+                = objectMapper.readValue(
+                copyToString(
+                        OrderControllerTest.class.getClassLoader()
+                                .getResourceAsStream("mock/GetPayment.json"
+                                ),
+                        defaultCharset()
+                ), OrderResponse.PaymentDetails.class
+        );
         paymentDetails.setPaymentStatus("SUCCESS");
 
-        OrderResponse.ProductDetails productDetails =
-                objectMapper.readValue(
-                        copyToString(
-                                OrderControllerTest.class.getClassLoader()
-                                        .getResourceAsStream("mock/GetProduct.json"),
-                                defaultCharset()
-                        ), OrderResponse.ProductDetails.class
-                );
+        OrderResponse.ProductDetails productDetails
+                = objectMapper.readValue(
+                copyToString(
+                        OrderControllerTest.class.getClassLoader()
+                                .getResourceAsStream("mock/GetProduct.json"),
+                        defaultCharset()
+                ), OrderResponse.ProductDetails.class
+        );
 
-        OrderResponse orderResponse =
-                OrderResponse.builder()
-                        .paymentDetails(paymentDetails)
-                        .productDetails(productDetails)
-                        .orderStatus(order.getOrderStatus())
-                        .orderDate(order.getOrderDate())
-                        .amount(order.getAmount())
-                        .orderId(order.getId())
-                        .build();
+        OrderResponse orderResponse
+                = OrderResponse.builder()
+                .paymentDetails(paymentDetails)
+                .productDetails(productDetails)
+                .orderStatus(order.getOrderStatus())
+                .orderDate(order.getOrderDate())
+                .amount(order.getAmount())
+                .orderId(order.getId())
+                .build();
         return objectMapper.writeValueAsString(orderResponse);
     }
 
-    @Test
-    public void test_When_Get_Order_Order_Not_Found() throws Exception {
-        MvcResult mvcResult =
-                mockMvc.perform(MockMvcRequestBuilders.get("/order/10")
-                                .with(jwt().authorities(new SimpleGrantedAuthority("Admin")))
-                                .contentType(MediaType.APPLICATION_JSON_VALUE))
-                        .andExpect(MockMvcResultMatchers.status().isNotFound())
-                        .andReturn();
-    }
 }
--- OrderService/src/test/java/com/msbeigi/orderservice/services/OrderServiceImplTest.java
+++ OrderService/src/test/java/com/msbeigi/orderservice/services/OrderServiceImplTest.java
@@ -36,23 +36,28 @@ public class OrderServiceImplTest {
 
     @Mock
     private OrderRepository orderRepository;
+
     @Mock
     private ProductService productService;
+
     @Mock
     private PaymentService paymentService;
+
     @Mock
     private RestTemplate restTemplate;
 
     @InjectMocks
     OrderService orderService = new OrderServiceImpl();
 
-    @DisplayName("Get order - success scenario")
+
+    @DisplayName("Get Order - Success Scenario")
     @Test
     void test_When_Order_Success() {
-        // Mocking
+        //Mocking
         Order order = getMockOrder();
         when(orderRepository.findById(anyLong()))
                 .thenReturn(Optional.of(order));
+
         when(restTemplate.getForObject(
                 "http://PRODUCT-SERVICE/product/" + order.getProductId(),
                 ProductResponse.class
@@ -63,12 +68,11 @@ public class OrderServiceImplTest {
                 PaymentResponse.class
         )).thenReturn(getMockPaymentResponse());
 
-        // Actual
+        //Actual
         OrderResponse orderResponse = orderService.getOrderDetailsById(1);
 
-        // Verification
-        verify(orderRepository, times(1))
-                .findById(anyLong());
+        //Verification
+        verify(orderRepository, times(1)).findById(anyLong());
         verify(restTemplate, times(1)).getForObject(
                 "http://PRODUCT-SERVICE/product/" + order.getProductId(),
                 ProductResponse.class);
@@ -76,65 +80,64 @@ public class OrderServiceImplTest {
                 "http://PAYMENT-SERVICE/payment/order/" + order.getId(),
                 PaymentResponse.class);
 
-        // Assert
+
+        //Assert
         assertNotNull(orderResponse);
         assertEquals(order.getId(), orderResponse.getOrderId());
     }
 
-    @DisplayName("Get Orders - Failure scenario")
+    @DisplayName("Get Orders - Failure Scenario")
     @Test
-    void test_When_Get_Order_Not_Found_Then_Not_Found() {
+    void test_When_Get_Order_NOT_FOUND_then_Not_Found() {
 
         when(orderRepository.findById(anyLong()))
-                .thenReturn(Optional.empty());
+                .thenReturn(Optional.ofNullable(null));
 
-        CustomException customException =
+        CustomException exception =
                 assertThrows(CustomException.class,
                         () -> orderService.getOrderDetailsById(1));
-        assertEquals("NOT_FOUND", customException.getErrorCode());
-        assertEquals(404, customException.getStatus());
+        assertEquals("NOT_FOUND", exception.getErrorCode());
+        assertEquals(404, exception.getStatus());
 
         verify(orderRepository, times(1))
                 .findById(anyLong());
-
     }
 
-    @DisplayName("Place order - Success Scenario")
+    @DisplayName("Place Order - Success Scenario")
     @Test
     void test_When_Place_Order_Success() {
-
         Order order = getMockOrder();
         OrderRequest orderRequest = getMockOrderRequest();
 
         when(orderRepository.save(any(Order.class)))
                 .thenReturn(order);
-        when(productService.reduceQuantity(anyLong(), anyLong()))
+        when(productService.reduceQuantity(anyLong(),anyLong()))
                 .thenReturn(new ResponseEntity<Void>(HttpStatus.OK));
         when(paymentService.doPayment(any(PaymentRequest.class)))
-                .thenReturn(new ResponseEntity<Long>(1L, HttpStatus.OK));
+                .thenReturn(new ResponseEntity<Long>(1L,HttpStatus.OK));
 
         long orderId = orderService.placeOrder(orderRequest);
 
         verify(orderRepository, times(2))
                 .save(any());
         verify(productService, times(1))
-                .reduceQuantity(anyLong(), anyLong());
+                .reduceQuantity(anyLong(),anyLong());
         verify(paymentService, times(1))
                 .doPayment(any(PaymentRequest.class));
 
         assertEquals(order.getId(), orderId);
-
     }
 
-    @DisplayName("Place Order - Payment fails scenario")
+    @DisplayName("Place Order - Payment Failed Scenario")
     @Test
-    void test_When_Place_Order_Payment_Fails_then_Order_Places() {
+    void test_when_Place_Order_Payment_Fails_then_Order_Placed() {
+
         Order order = getMockOrder();
         OrderRequest orderRequest = getMockOrderRequest();
 
         when(orderRepository.save(any(Order.class)))
                 .thenReturn(order);
-        when(productService.reduceQuantity(anyLong(), anyLong()))
+        when(productService.reduceQuantity(anyLong(),anyLong()))
                 .thenReturn(new ResponseEntity<Void>(HttpStatus.OK));
         when(paymentService.doPayment(any(PaymentRequest.class)))
                 .thenThrow(new RuntimeException());
@@ -144,17 +147,15 @@ public class OrderServiceImplTest {
         verify(orderRepository, times(2))
                 .save(any());
         verify(productService, times(1))
-                .reduceQuantity(anyLong(), anyLong());
+                .reduceQuantity(anyLong(),anyLong());
         verify(paymentService, times(1))
                 .doPayment(any(PaymentRequest.class));
 
         assertEquals(order.getId(), orderId);
-
     }
 
     private OrderRequest getMockOrderRequest() {
-        return OrderRequest
-                .builder()
+        return OrderRequest.builder()
                 .productId(1)
                 .quantity(10)
                 .paymentMode(PaymentMode.CASH)
@@ -163,8 +164,7 @@ public class OrderServiceImplTest {
     }
 
     private PaymentResponse getMockPaymentResponse() {
-        return PaymentResponse
-                .builder()
+        return PaymentResponse.builder()
                 .paymentId(1)
                 .paymentDate(Instant.now())
                 .paymentMode(PaymentMode.CASH)
@@ -175,11 +175,10 @@ public class OrderServiceImplTest {
     }
 
     private ProductResponse getMockProductResponse() {
-        return ProductResponse
-                .builder()
+        return ProductResponse.builder()
                 .productId(2)
                 .productName("iPhone")
-                .price(400)
+                .price(100)
                 .quantity(200)
                 .build();
     }
